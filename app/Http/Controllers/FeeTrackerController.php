94db18d1024fe1403f1211f75f86165f6d07a250
muhammad nabil <nabilrhm94@gmail.com>
2022-06-18 23:52:19 +0800

    add commenting and place the waapify to blast the notification

--- app/Http/Controllers/FeeTrackerController.php
+++ app/Http/Controllers/FeeTrackerController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\FeeTracker;
 use App\User;
+use Auth;
+use DB;
+use File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use DB;
-use Validator,Redirect,Response,File,Auth;
+use Redirect;
+use Response;
 
 class FeeTrackerController extends Controller
 {
@@ -16,57 +20,50 @@ class FeeTrackerController extends Controller
     }
     public function index()
     {
-        if(Auth::User()->role =='guardian'){
-        $feetracker = FeeTracker::all()
-        ->where('guardian_id', auth()->user()->id);}
+        if (Auth::User()->role == 'guardian') {
+            $feetracker = FeeTracker::all()
+                ->where('guardian_id', auth()->user()->id);}
         /*->orderBy('day_id', 'asc')
         ->orderBy('time_from', 'asc')
         ->get();*/
-        else if(Auth::User()->role =='admin'){
+        else if (Auth::User()->role == 'admin') {
             $feetracker = FeeTracker::all();}
-  
-        return view('feetrackers.index',compact('feetracker'));
+
+        return view('feetrackers.index', compact('feetracker'));
     }
 
     public function create()
     {
-       $guardians = DB::select('select* from users where role ="guardian"');
-        return view('feetrackers.create',['guardians'=>$guardians]);        
+        $guardians = DB::select('select* from users where role ="guardian"');
+        return view('feetrackers.create', ['guardians' => $guardians]);
     }
 
     public function store(Request $request)
     {
 
         request()->validate([
-            'fee_month'=> 'nullable',
-            'payment_deadline'=>'nullable',
-            'file' => 'nullable',
-            'payment_status'=>'nullable',
+            'fee_month'        => 'nullable',
+            'payment_deadline' => 'nullable',
+            'file'             => 'nullable',
+            'payment_status'   => 'nullable',
 
         ]);
-            
-         foreach($request->userid as $value => $task)
-            {
-                $data = DB::table('fee_trackers')->insert([
-                    'fee_month'=>$request->fee_month,
-                    'payment_deadline'=>$request->payment_deadline,
-                    'guardian_id'=>$request->userid[$value],
-                    'payment_status'=>$request->payment_status,
-                ]);
-            }
 
-           
-        
-        
-        
-        
+        foreach ($request->userid as $value => $task) {
+            $data = DB::table('fee_trackers')->insert([
+                'fee_month'        => $request->fee_month,
+                'payment_deadline' => $request->payment_deadline,
+                'guardian_id'      => $request->userid[$value],
+                'payment_status'   => $request->payment_status,
+            ]);
+        }
 
         //FeeTracker::create($request->all());
         return back()
-                ->with('success','Image Upload Successfully');
+            ->with('success', 'Image Upload Successfully');
 
     }
-     /**
+    /**
      * Display the specified resource.
      *
      * @param  \App\FeeTracker  $feetrackers
@@ -74,7 +71,7 @@ class FeeTrackerController extends Controller
      */
     public function show(FeeTracker $feetracker)
     {
-        return view('feetrackers.show',compact('feetracker'));
+        return view('feetrackers.show', compact('feetracker'));
     }
 
     /**
@@ -83,12 +80,12 @@ class FeeTrackerController extends Controller
      * @param  \App\FeeTracker  $feetrackers
      * @return \Illuminate\Http\Response
      */
-    public function edit(  $feetracker)
+    public function edit($feetracker)
     {
         $feetrackers = DB::table('fee_trackers')
-        ->where('id','=',$feetracker)->get();
-        
-        return view('feetrackers.edit',compact('feetrackers'));
+            ->where('id', '=', $feetracker)->get();
+
+        return view('feetrackers.edit', compact('feetrackers'));
     }
 
     /**
@@ -98,43 +95,49 @@ class FeeTrackerController extends Controller
      * @param  \App\FeeTracker  $feetracker
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,  $feetracker)
+    public function update(Request $request, $feetracker)
     {
         request()->validate([
-            'fee_month'=> 'nullable',
-            'file' => 'nullable',
-            'payment_status'=>'nullable',
+            'fee_month'      => 'nullable',
+            'file'           => 'nullable',
+            'payment_status' => 'nullable',
 
         ]);
-            if($request->hasFile('receipt'))
-            {
-                
-            $file = $request->file('receipt');
-            $fileName =$file ->getClientOriginalName();
-            $destinationPath = public_path().'/receipts';
-            $file->move($destinationPath,$fileName);
+        if ($request->hasFile('receipt')) {
 
-            
+            $file            = $request->file('receipt');
+            $fileName        = $file->getClientOriginalName();
+            $destinationPath = public_path() . '/receipts';
+            $file->move($destinationPath, $fileName);
 
-            DB::table('fee_trackers')->where('id',$feetracker)->update([
-                
-                'receipt'=>$fileName,
-                'payment_status'=>$request->payment_status,
+            DB::table('fee_trackers')->where('id', $feetracker)->update([
+
+                'receipt'        => $fileName,
+                'payment_status' => $request->payment_status,
             ]);
-        
-        
-        
+
+            // sini bole letak utk notification teachers
+            $message     = "hello world";
+            $phone_no    = "0133047981";
+            $instance_id = '62A220F4E788D';
+            $token       = '76bad889dad35fa4fa80acb45f2460a4';
+
+            $url = Http::post('https://pro.waapify.com/api/send.php?number=6' . $phone_no . '&type=text&message=' . $message . '&instance_id=' . $instance_id . '&access_token=' . $token);
+
+            // utk $url ni xyah return apa2.. just terus ke page.. nnti dia akan auto send notification
+            // variable atas tu qila boleh guna klau nak tuka no org lain or any message.
+
         }
 
         //FeeTracker::create($request->all());
         return back()
-                ->with('success','Image Upload Successfully');
-       /* $request->validate([
-            'payment_status'=>'nullable',
-        ]);
-  
-        $feetracker->update($request->all());*/
-  
+            ->with('success', 'Image Upload Successfully');
+        /* $request->validate([
+    'payment_status'=>'nullable',
+    ]);
+
+    $feetracker->update($request->all());*/
+
     }
 
     /**
@@ -143,23 +146,29 @@ class FeeTrackerController extends Controller
      * @param  \App\FeeTracker $feetracker
      * @return \Illuminate\Http\Response
      */
-    public function destroy( $feetracker)
+    public function destroy($feetracker)
     {
-        
+
         $f_tracker = FeeTracker::find($feetracker);
         $f_tracker->delete();
 
-       return redirect()->route('fee_trackers.index')
-       ->with('success','Fee Tracker deleted successfully');
+        return redirect()->route('fee_trackers.index')
+            ->with('success', 'Fee Tracker deleted successfully');
     }
     public function text()
     {
-    	
-        $url = Http::post('https://pro.waapify.com/api/send.php?number=60133047981&type=text&message=guardian%20uploaded%20receipt%20in%20fee%20tracker&instance_id=62A220F4E788D&access_token=76bad889dad35fa4fa80acb45f2460a4');
-        
+
+        // $url = Http::post('https://pro.waapify.com/api/send.php?number=60133047981&type=text&message=guardian%20uploaded%20receipt%20in%20fee%20tracker&instance_id=62A220F4E788D&access_token=76bad889dad35fa4fa80acb45f2460a4');
+
+        $message     = "hello world";
+        $phone_no    = "0133047981";
+        $instance_id = '62A220F4E788D';
+        $token       = '76bad889dad35fa4fa80acb45f2460a4';
+
+        $url = Http::post('https://pro.waapify.com/api/send.php?number=6' . $phone_no . '&type=text&message=' . $message . '&instance_id=' . $instance_id . '&access_token=' . $token);
+
         //return Redirect::to('https://www.itsolutionstuff.com/post/how-to-send-email-to-multiple-users-in-laravelexample.html');
-        redirect()->to($url)->send();
-    	
+        // redirect()->to($url)->send();
+
     }
 }
-
